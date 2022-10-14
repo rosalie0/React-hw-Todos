2d794e7c22b4f9c80777ff54185ab97257bff1a4
Rosalie Newman <rosalienewman0@gmail.com>
2022-10-14 01:36:15 -0400

    added a 'done' state for each task component as well as styles that, depending on if it is done or not, are applied. styles applied include a check mark in the button, a lower of opacity for the whole task element, and a strikethrough of the task's text.

--- src/components/SingleTodo.jsx
+++ src/components/SingleTodo.jsx
@@ -1,15 +1,32 @@
 //  Represents a single Todo item
-import React from 'react';
+import React, { useState } from 'react';
+
+const strikeStyle = {
+	textDecorationLine: 'line-through',
+};
+
+const lowerOpacityStyle = {
+	opacity: '20%',
+};
 
 const SingleToDo = ({ todoName }) => {
+	//State of if task completed or not.
+	// false = not done. true = done
+	const [done, setDone] = useState(false);
+
 	function clickToDo() {
-		console.log('clicked button!');
+		setDone(!done); //toggle it
 	}
 
 	return (
-		<div className="single-todo-div">
-			<input type="button" className="todoCheckbox" onClick={clickToDo} />
-			{todoName}
+		<div className="single-todo-div" style={done ? lowerOpacityStyle : null}>
+			<input
+				type="button"
+				className="todoCheckbox"
+				onClick={clickToDo}
+				value={done ? '✔' : ''} //If done, gets a checkmark
+			/>
+			<div style={done ? strikeStyle : null}>{todoName}</div>
 		</div>
 	);
 };
